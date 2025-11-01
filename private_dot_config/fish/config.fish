54453d02445fbe0edef1cd84b961bc6466f790de
Eric Ottosson <contact@ericotsn.com>
2025-11-01 15:00:41 +0100

    Major refactoring #2

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,9 +1,21 @@
 if status is-interactive
-    alias ls="lsd"
-    alias ll="lsd -l"
-    alias la="lsd -la"
-    alias lt="lsd --tree"
+    if type -q lsd
+        alias ls="lsd --almost-all --long"
+        alias lt="lsd --tree"
+    end
 
-    fzf --fish | source
-    zoxide init fish | source
+    if type -q zoxide
+        zoxide init fish | source
+    end
+
+    if type -q fzf
+        set -Ux FZF_DEFAULT_OPTS "\
+        --color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
+        --color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+        --color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+        --color=selected-bg:#494D64 \
+        --color=border:#6E738D,label:#CAD3F5"
+
+        fzf --fish | source
+    end
 end
