224ed44d4638ebfe5ccddcff004c2af586bb6bf4
Eric Ottosson <contact@ericotsn.com>
2026-02-28 01:00:10 +0100

    Housekeeping

--- home/dot-config/nvim/lua/plugins/mini.lua
+++ home/dot-config/nvim/lua/plugins/mini.lua
@@ -1,62 +1,14 @@
 return {
-  {
-    "nvim-mini/mini.ai",
-    version = false,
-    config = function()
-      require("mini.ai").setup()
-    end,
-    cond = vim.g.vscode or true,
-  },
-  {
-    "nvim-mini/mini.comment",
-    version = false,
-    config = function()
-      require("mini.comment").setup()
-    end,
-    cond = vim.g.vscode or true,
-  },
-  {
-    "nvim-mini/mini.jump",
-    version = false,
-    config = function()
-      require("mini.jump").setup()
-    end,
-    cond = vim.g.vscode or true,
-  },
-  {
-    "nvim-mini/mini.move",
-    version = false,
-    config = function()
-      require("mini.move").setup({
-        mappings = {
-          left = "<C-h>",
-          right = "<C-l>",
-          down = "<C-j>",
-          up = "<C-k>",
-
-          line_left = "<C-h>",
-          line_right = "<C-l>",
-          line_down = "<C-j>",
-          line_up = "<C-k>",
-        },
-      })
-    end,
-    cond = vim.g.vscode or true,
-  },
-  {
-    "nvim-mini/mini.pairs",
-    version = false,
-    config = function()
-      require("mini.pairs").setup()
-    end,
-    cond = vim.g.vscode or true,
-  },
-  {
-    "nvim-mini/mini.surround",
-    version = false,
-    config = function()
-      require("mini.surround").setup()
-    end,
-    cond = vim.g.vscode or true,
-  },
+  "nvim-mini/mini.nvim",
+  version = false,
+  config = function()
+    require("mini.ai").setup()
+    require("mini.basics").setup()
+    require("mini.comment").setup()
+    require("mini.jump").setup()
+    require("mini.move").setup()
+    require("mini.pairs").setup()
+    require("mini.surround").setup()
+  end,
+  cond = vim.g.vscode or true,
 }
