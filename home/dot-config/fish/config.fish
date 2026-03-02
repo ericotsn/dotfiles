454adefa7d07c568eac007f499794b36465cff3d
Eric Ottosson <contact@ericotsn.com>
2026-03-02 12:06:00 +0100

    Set up Lazygit inside Visual Studio Code

--- home/dot-config/fish/config.fish
+++ home/dot-config/fish/config.fish
@@ -1,5 +1,9 @@
-set -gx EDITOR "nvim"
-set -gx MANPAGER "nvim +Man!"
+if string match -q -- "*vscode*" $TERM_PROGRAM
+	set -x VISUAL "code --wait"
+else
+	set -x EDITOR "nvim"
+	set -x MANPAGER "nvim +Man!"
+end
 
 fish_add_path \
 	$HOME/.bin \
