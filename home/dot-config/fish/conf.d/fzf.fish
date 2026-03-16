3f1a1727430fb7b0dd3f12ebe0134343af90fbee
Eric Ottosson <contact@ericotsn.com>
2026-03-16 08:47:18 +0100

    Switch colorscheme to Catppuccin Latte

--- home/dot-config/fish/conf.d/fzf.fish
+++ home/dot-config/fish/conf.d/fzf.fish
@@ -1,8 +1,9 @@
 fzf --fish | source
 
+# https://github.com/catppuccin/fzf
 set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
---color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
---color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
---color=selected-bg:#494D64 \
---color=border:#6E738D,label:#CAD3F5"
+--color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+--color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+--color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+--color=selected-bg:#BCC0CC \
+--color=border:#9CA0B0,label:#4C4F69"
