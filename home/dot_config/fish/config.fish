a6820f8297b20a05a9c6b261dfdc1b23fbbfbc62
Eric Ottosson <contact@ericotsn.com>
2026-07-30 17:19:39 +0200

    Remove VSCode settings

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,10 +1,5 @@
+set -x EDITOR 'nvim'
+set -x MANPAGER 'nvim +Man!'
 set -x XDG_CONFIG_HOME "$HOME/.config"
-
-if string match -q -- '*vscode*' $TERM_PROGRAM
-  set -x VISUAL 'code --wait'
-else
-  set -x EDITOR 'nvim'
-  set -x MANPAGER 'nvim +Man!'
-end
 
 fish_add_path -g ~/.local/bin ~/bin
