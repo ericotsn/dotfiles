ae4a81e179ca392f34237f7fc9126a17f65236d7
Eric Ottosson <contact@ericotsn.com>
2026-08-29 11:30:14 +0200

    Format code in Neovim using conform.nvim

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -208,10 +208,10 @@ require("mini.pick").setup()
 require("mini.icons").setup()
 MiniIcons.tweak_lsp_kind()
 
-require("mini.completion").setup({
+require("mini.completion").setup {
   -- Effectively disable signature help.
-  delay = { signature = 10^7 },
-})
+  delay = { signature = 10 ^ 7 },
+}
 
 require("mini.surround").setup {
   mappings = {
@@ -295,6 +295,32 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 -- }}}2 // Mason
+
+-- Conform --------------------------------------------------------------- {{{2
+
+vim.pack.add { "https://github.com/stevearc/conform.nvim" }
+
+require("conform").setup {
+  formatters_by_ft = {
+    lua = { "stylua" },
+  },
+}
+
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format { async = true, lsp_format = "fallback", range = range }
+end, { range = true })
+
+vim.keymap.set({ "n", "v" }, "<Leader>F", "<Cmd>Format<CR>")
+
+-- }}}2 // Conform
 
 -- }}}1 // Plugins
 
