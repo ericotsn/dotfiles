b08d11627bbd2e9b66231d5219db081d9ea23f6f
Eric Ottosson <contact@ericotsn.com>
2026-08-30 16:10:38 +0200

    Move Neovim lazygit integration into its own file/plugin

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -84,46 +84,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 
 -- }}}1 // Autocommands
 
--- Functions ============================================================= {{{1
-
-local function open_lazygit()
-  local cwd = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) or vim.fn.getcwd()
-
-  vim.cmd "tabnew"
-  local lazygit_tab = vim.api.nvim_get_current_tabpage()
-  local lazygit_buf = vim.api.nvim_get_current_buf()
-
-  vim.opt_local.number = false
-  vim.opt_local.relativenumber = false
-  vim.opt_local.signcolumn = "no"
-  vim.opt_local.statuscolumn = ""
-
-  local job_id = vim.fn.jobstart({ "lazygit" }, {
-    cwd = cwd,
-    term = true,
-    on_exit = function()
-      vim.schedule(function()
-        if vim.api.nvim_tabpage_is_valid(lazygit_tab) then
-          vim.cmd("tabclose " .. vim.api.nvim_tabpage_get_number(lazygit_tab))
-        end
-        if vim.api.nvim_buf_is_valid(lazygit_buf) then
-          vim.api.nvim_buf_delete(lazygit_buf, { force = true })
-        end
-      end)
-    end,
-  })
-
-  if job_id <= 0 then
-    vim.cmd "tabclose"
-    vim.notify("Could not start Lazygit", vim.log.levels.ERROR)
-  end
-end
-
--- }}}1 // Functions
-
 -- Keymaps =============================================================== {{{1
-
-vim.keymap.set("n", "<Leader>gg", open_lazygit, { silent = true })
 
 -- Move by display lines when no count is provided.
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
